04aa94741d4c8b9e0287efb85b9221cdb11a66d1
svmihar <svmihar@gmail.com>
2020-06-10 20:49:06 +0700

    :sparkles:

--- dedup.py
+++ dedup.py
@@ -1,33 +1,30 @@
 from PIL import Image
 import imagehash
-from collections import ChainMap
 from pathlib import Path
 from joblib import dump, load
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 from itertools import combinations
 
+images = [str(a) for a in Path("./images").iterdir()]
 """
-def hasher(image_path): 
+def hasher(image_path):
     img = Image.open(image_path)
     img_hash = imagehash.phash(img)
     return {'image':str(image_path), 'hash': img_hash}
 
-with ThreadPoolExecutor(max_workers=20) as sp: 
+with ThreadPoolExecutor(max_workers=20) as sp:
     result = sp.map(hasher, images)
 
 ha = []
-for a in result: 
-    try: 
+for a in result:
+    try:
         ha.append(a)
-    except: 
+    except:
         pass
 
-breakpoint()
 dump(ha, 'sertif_hashes.pkl')
-breakpoint()
 """
-images = [str(a) for a in Path("./images").iterdir()]
 images_hash = load("sertif_sama_fix.pkl")
 hasil = {}
 for img in images_hash:
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-- [ ] satuin gambar, dengan cara cari seluruh format gambarnya pake glob, terus pindahin ke satu folder aja 
+- [x] satuin gambar, dengan cara cari seluruh format gambarnya pake glob, terus pindahin ke satu folder aja 
+- [x] search duplicated dataset with phash
+
+
+see you in a month. lol.
+![training has begun see you in 10 days](https://i.imgur.com/Bh2i0zU.png)
