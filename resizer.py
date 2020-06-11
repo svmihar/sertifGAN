068112ce9a1a0d576b18b3853da86e830bcad723
svmihar <svmihar@gmail.com>
2020-06-11 10:04:44 +0700

    :zap: transparent layer remover

--- resizer.py
+++ resizer.py
@@ -1,18 +1,29 @@
-from PIL import Image
+from PIL import Image, ImageCms
 from tqdm import tqdm
 from pathlib import Path
 
-images = [str(a) for a in Path('./images').iterdir()]
+images = [str(a) for a in Path('./images/resized').iterdir()]
 
 R = 512
+
+def resize(img, size=(R,R)): 
+    new = im.resize(size, Image.ANTIALIAS)
+    return new
+
+
+def convert_to_srgb(img):
+    '''convert pil image to plain old jpg no transparency'''
+    rgb = img.convert('RGB')
+    return rgb
 
 for img in tqdm(images):
     try:
         im = Image.open(img)
-        new = im.resize((R,R), Image.ANTIALIAS)
-        new.save(img.split('/')[-1].lower())
+        new = convert_to_srgb(im)
+        new.save('./images/resized_rgb/'+img.split('/')[-1].lower())
     except Exception as e:
         print(img)
         print(e)
         pass
+
 
